6c05e419f5916d8d236adefb8709fd9f7940323a
jsmmns <joe.simmons@shakepay.co>
2025-11-25 15:52:36 -0500

    mess up spiral matrix impl

--- daily/answers/54.spiral-matrix.js
+++ daily/answers/54.spiral-matrix.js
@@ -19,21 +19,7 @@ var spiralOrder = function(matrix) {
   let right = matrix[0].length - 1;
 
   while (true) {
-    for (let i = left; i <= right; i++) res.push(matrix[top][i]);
-    top++;
-    if (top > bottom) break;
 
-    for (let i = top; i <= bottom; i++) res.push(matrix[i][right]);
-    right--;
-    if (left > right) break;
-
-    for (let i = right; i >= left; i--) res.push(matrix[bottom][i]);
-    bottom--;
-    if (top > bottom) break;
-
-    for (let i = bottom; i >= top; i--) res.push(matrix[i][left]);
-    left++;
-    if (left > right) break;
   }
 
   return res;
